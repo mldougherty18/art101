7acf9164dced3994cc65ff4c24d8a497c78f051c
mldougherty18 <146750128+mldougherty18@users.noreply.github.com>
2023-11-17 14:47:50 -0800

    Create lab.js

---
+++ CLASS THING DIDNT WORK/js/lab.js
@@ -0,0 +1,10 @@
+// index.js - Understand jquery and button functions
+// Author: Maggie Dougherty
+// Date: nov 8 2023
+
+//some code is thanks to the examples in the lab and some is from ChatGPT
+
+console.log("javascript is working")
+
+$("#my-button").click(function(){
+  console.log("button clicked!")});
