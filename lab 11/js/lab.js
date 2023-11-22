ce56e3ee029a897c49b9fc11e6decc113c4174bd
mldougherty18 <146750128+mldougherty18@users.noreply.github.com>
2023-11-22 15:18:14 -0800

    Update lab.js

--- lab 11/js/lab.js
+++ lab 11/js/lab.js
@@ -1,26 +1,23 @@
-// index.js - create a chat generator 
+// index.js - his simple JavaScript/jQuery script gets a value from an input field and outputs a modified version. jquery must load the correctly sorted input
 // Author: Maggie Dougherty
-// Date: nov 15 2023
+// Date: nov 20 2023
 
-//find the element adn give it a function 
-function generateRandomText() {
-  const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-  const min = 3;
-  const max = 100;
-  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
-  // Get a random starting index to slice the Lorem Ipsum text
-  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
-  // Generate the random Lorem Ipsum-like text
-  return text.slice(randStart, randStart + randLen);
+// Sorts the characters of a string in alphabetical order.
+function sortString(inputString) {
+  // We have to convert our string to an array and back again to sort it
+  return inputString.split('').sort().join('');
 }
 
-
 // click listener for button
-$("#make-convo").click(function(){
-  // Generate random text using the function
-  const newText = generateRandomText();
- 
-  // Append a new div to the output div
-  $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+$("#submit").click(function () {
+  // get value of input field when the button is clicked
+  const userName = $("#user-name").val();
+
+  // now let's sort it
+  const userNameSorted = sortString(userName);
+
+  // append a new div to our output div
+  $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
 });
-//credit to Wes Modes and chatgpt
+
+//Thank you wes modes, wesbot, and chatgpt!!!
