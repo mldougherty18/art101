48589d1c6bc10af74e160552bee853967a7de64d
mldougherty18 <146750128+mldougherty18@users.noreply.github.com>
2023-11-29 15:22:51 -0800

    Update lab.js

--- lab 14/js/lab.js
+++ lab 14/js/lab.js
@@ -2,54 +2,34 @@
 // Author: Maggie Dougherty
 // Date: nov 29 2023
 
+// Creating a function to compare the strings and sort them alphabetically regardless of case
 // Credit to Stack Overflow - How Do You Sort Letters in JavaScript with Capital & Lowercase Letters Combined?
-function caseSensitive(stringA, stringB) {
-  return stringA.toLowerCase().localeCompare(stringB.toLowerCase());
+function caseSensitive(stringA,_string ) {
+  return stringA. toLowerCase(). localeCompare(stringA. toLowerCase());
 }
 
 //create a function to get user input and sort their name
 //credit to Wes Modes ART101 lab 7 canvas page and Percilla for helping us! 
-function sortingUserName() {
+function sortingUserName(){
   //creating a variable to get user input
-  var userName = window.prompt("hello world. what is your name?");
-
-  // Check if the user clicked "Cancel" or closed the prompt
-  if (userName === null) {
-    console.log("User canceled the prompt");
-    return null; // Or handle it in a way that makes sense for your application
-  }
-
-  console.log("userName = ", userName);
-
+  var userName = window.prompt ("hello world. what is your name?");
+  console. log ("userName = ", userName);
   //splitting string into array 
   var nameArray = userName.split('');
-  console.log("nameArray = ", nameArray);
-
-  //sorting array using function - caseSensitive
-  var caseSens = nameArray.sort(caseSensitive);
-  console.log("caseSens = ", caseSens);
-
+  console.log ("nameArray = ", nameArray);
+  //sorting array 
+  var nameSortArray = nameArray.sort ();
+  console.log ("nameSortArray = ", nameSortArray);
+  //using function - caseSenstitive to sort through string 
+  var caseSens = nameSortArray.sort (caseSensitive);
+  console.log ("caseSens = ", caseSens) ;
   //joining array into string 
-  var nameSorted = caseSens.join('');
-  console.log("nameSorted = ", nameSorted);
-
+  var nameSorted = caseSens. join('');
+  console.log ("nameSorted = ", nameSorted);
   // returning the sorted name 
   return nameSorted;
 }
 
 //output
-var sortedName = sortingUserName();
-
-// Check if sortedName is not null before using it
-if (sortedName !== null) {
-  // Create a paragraph element
-  var resultParagraph = document.createElement('p');
-
-  // Set the content of the paragraph
-  resultParagraph.textContent = "I fixed your name: " + sortedName;
-
-  // Append the paragraph to the body or another appropriate container
-  document.body.appendChild(resultParagraph);
-} else {
-  console.log("You canceled the prompt. No name to fix.");
-}
+document.writeln("I fixed your name: ",
+sortingUserName (),"</br>");
