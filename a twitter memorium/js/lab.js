e16987099801a5dc74cb9b9093f098c511c720f6
mldougherty18 <146750128+mldougherty18@users.noreply.github.com>
2023-11-29 14:49:20 -0800

    Update lab.js

--- a twitter memorium/js/lab.js
+++ a twitter memorium/js/lab.js
@@ -15,4 +15,33 @@ function showPage(pageName) {
   if (selectedSection) {
     selectedSection.style.display = 'block';
   }
-}
+}const Twit = require('twit');
+
+const T = new Twit({
+  consumer_key: 'your_consumer_key',
+  consumer_secret: 'your_consumer_secret',
+  access_token: 'your_access_token',
+  access_token_secret: 'your_access_token_secret',
+});
+T.get('statuses/user_timeline', { screen_name: 'twitter', count: 200 }, function (err, data, response) {
+  if (err) {
+    console.error(err);
+  } else {
+    // Process the tweets (e.g., store in a file or database)
+    console.log(data);
+  }
+});
+const fs = require('fs');
+
+fs.writeFileSync('tweets.json', JSON.stringify(data), 'utf-8');
+// Fetch and display tweets
+fetch('tweets.json')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(tweet => {
+      const tweetElement = document.createElement('p');
+      tweetElement.textContent = tweet.text;
+      tweetsContainer.appendChild(tweetElement);
+    });
+  })
+  .catch(error => console.error(error));
