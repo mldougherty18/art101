5126b62cc0f10de1ea2c2467564d1c945d2f8afd
mldougherty18 <146750128+mldougherty18@users.noreply.github.com>
2023-12-01 15:41:07 -0800

    Update lab.js

--- lab 14/js/lab.js
+++ lab 14/js/lab.js
@@ -2,34 +2,30 @@
 // Author: Maggie Dougherty
 // Date: nov 29 2023
 
-// Creating a function to compare the strings and sort them alphabetically regardless of case
-// Credit to Stack Overflow - How Do You Sort Letters in JavaScript with Capital & Lowercase Letters Combined?
-function caseSensitive(stringA,_string ) {
-  return stringA. toLowerCase(). localeCompare(stringA. toLowerCase());
-}
-
-//create a function to get user input and sort their name
-//credit to Wes Modes ART101 lab 7 canvas page and Percilla for helping us! 
-function sortingUserName(){
-  //creating a variable to get user input
-  var userName = window.prompt ("hello world. what is your name?");
-  console. log ("userName = ", userName);
-  //splitting string into array 
-  var nameArray = userName.split('');
-  console.log ("nameArray = ", nameArray);
-  //sorting array 
-  var nameSortArray = nameArray.sort ();
-  console.log ("nameSortArray = ", nameSortArray);
-  //using function - caseSenstitive to sort through string 
-  var caseSens = nameSortArray.sort (caseSensitive);
-  console.log ("caseSens = ", caseSens) ;
-  //joining array into string 
-  var nameSorted = caseSens. join('');
-  console.log ("nameSorted = ", nameSorted);
-  // returning the sorted name 
-  return nameSorted;
-}
-
-//output
-document.writeln("I fixed your name: ",
-sortingUserName (),"</br>");
+// Using the core $.ajax() method
+$.ajax({
+  // The URL for the request (from the api docs)
+  url: "https://yourapiendpoint.com/",
+  // The data to send (will be converted to a query string)
+  data: { 
+          // here is where any data required by the api 
+          //   goes (check the api docs)
+          id: 123,
+          api_key: "blahblahblah",
+        },
+  // Whether this is a POST or GET request
+  type: "GET",
+  // The type of data we expect back
+  dataType : "json",
+  // What do we do when the api call is successful
+  //   all the action goes in here
+  success: function(data) {
+      // do stuff
+      console.log(data);
+  },
+  // What we do if the api call fails
+  error: function (jqXHR, textStatus, errorThrown) { 
+      // do stuff
+      console.log("Error:", textStatus, errorThrown);
+  }
+})
