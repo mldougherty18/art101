9330e1e8a997fd2be13c00a8bf1a861b55d0f2dc
mldougherty18 <146750128+mldougherty18@users.noreply.github.com>
2023-12-01 15:41:44 -0800

    Create lab.js

---
+++ lab 15/js/lab.js
@@ -0,0 +1,29 @@
+// index.js - debug
+// Author: Maggie Dougherty
+// Date: nov 29 2023
+
+// jQuery code to handle button click and make AJAX call
+$(document).ready(function() {
+  $("#activate").on("click", function() {
+      // Make an AJAX call to XKCD API
+      $.ajax({
+          url: "https://xkcd.com/info.0.json",
+          method: "GET",
+          dataType: "json",
+          success: function(data) {
+              // Create HTML content with specific information
+              var outputHtml = `
+                  <h2>${data.title}</h2>
+                  <img src="${data.img}" alt="${data.alt}" style="max-width: 100%; height: auto;">
+                  <p>${data.alt}</p>
+              `;
+              // Display the HTML content in the output div
+              $("#output").html(outputHtml);
+          },
+          error: function(xhr, status, error) {
+              console.log("Error fetching data:", status, error);
+              $("#output").text("Error fetching data");
+          }
+      });
+  });
+});
