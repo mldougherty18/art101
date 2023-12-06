7830ebbc2c0bb2c58808b2611359570946588ca9
mldougherty18 <146750128+mldougherty18@users.noreply.github.com>
2023-12-06 15:34:40 -0800

    Update lab.js

--- a twitter memorium/js/lab.js
+++ a twitter memorium/js/lab.js
@@ -2,46 +2,44 @@
 // Author: Maggie Dougherty
 // Date: oct 25 2023
 
-// Constants
-function showPage(pageName) {
-  // Hide all content sections
-  var contentSections = document.querySelectorAll('.content-section');
-  contentSections.forEach(function(section) {
-    section.style.display = 'none';
+// Replace these with your actual API keys
+const apiKey = 'YveC5l4BBFeKo1vBG7vyV08Bh';
+const apiSecretKey = '88uLS2bNdofNLIUo23NQcApWbN3BYOzkfaOuen1rM1UUwmYTvp';
+const accessToken = '1724560378179567616-mGpwBr68ZLhlXD2Pb9NcjQMyYBu7fe';
+const accessTokenSecret = 'do1i4D5ReT8z4HWi695Rll9JDy3puwCe5P8s0S9GsFPNz';
+
+// Twitter username
+const twitterUsername = '1732539841571909632doughlady42';
+
+// Number of tweets to fetch
+const tweetCount = 5;
+
+// Function to fetch tweets
+async function fetchTweets() {
+  const response = await fetch(`https://api.twitter.com/2/tweets?screen_name=${twitterUsername}&tweet.fields=created_at&max_results=${tweetCount}`, {
+    headers: {
+      'Authorization': `Bearer ${apiKey} ${apiSecretKey}`
+    }
   });
 
-  // Show the selected content section
-  var selectedSection = document.getElementById(pageName);
-  if (selectedSection) {
-    selectedSection.style.display = 'block';
-  }
-}const Twit = require('twit');
+  const data = await response.json();
+  return data.data;
+}
 
-const T = new Twit({
-  consumer_key: 'your_consumer_key',
-  consumer_secret: 'your_consumer_secret',
-  access_token: 'your_access_token',
-  access_token_secret: 'your_access_token_secret',
-});
-T.get('statuses/user_timeline', { screen_name: 'twitter', count: 200 }, function (err, data, response) {
-  if (err) {
-    console.error(err);
-  } else {
-    // Process the tweets (e.g., store in a file or database)
-    console.log(data);
-  }
-});
-const fs = require('fs');
+// Function to display tweets
+async function displayTweets() {
+  const tweetsContainer = document.getElementById('tweets-container');
+  const tweets = await fetchTweets();
 
-fs.writeFileSync('tweets.json', JSON.stringify(data), 'utf-8');
-// Fetch and display tweets
-fetch('tweets.json')
-  .then(response => response.json())
-  .then(data => {
-    data.forEach(tweet => {
-      const tweetElement = document.createElement('p');
-      tweetElement.textContent = tweet.text;
-      tweetsContainer.appendChild(tweetElement);
-    });
-  })
-  .catch(error => console.error(error));
+  tweetsContainer.innerHTML = '<h2>Latest Tweets:</h2>';
+  
+  tweets.forEach(tweet => {
+    const tweetElement = document.createElement('p');
+    tweetElement.textContent = `${tweet.text} - ${tweet.created_at}`;
+    tweetsContainer.appendChild(tweetElement);
+  });
+}
+
+// Fetch and display tweets when the page loads
+displayTweets();
+</script>
